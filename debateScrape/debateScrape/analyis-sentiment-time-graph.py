60bdb3fae42931443457afa81b2f37a956eccd20
keelanfh <info@keelanfh.com>
2017-01-02 14:12:53 +0000

    start pypy

---
+++ debateScrape/debateScrape/analyis-sentiment-time-graph.py
@@ -0,0 +1,13 @@
+# This creates two graphs, but only one is shown with the data for both positive and negative words.
+# I don't know why this happens...
+# Red is positive and blue is negative
+
+# plt.plot(uniqueYears, uniquepositivewords, 'ro')
+# plt.xlabel('Year')
+# plt.ylabel('Positive Words')
+# plt.show()
+#
+# plt.plot(uniqueYears, uniquenegativewords, 'bo')
+# plt.xlabel('Year')
+# plt.ylabel('Negative Words')
+# plt.show()
