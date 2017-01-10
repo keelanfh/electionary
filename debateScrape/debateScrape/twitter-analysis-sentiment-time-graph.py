221293965c531125ed8c7b796ba8c49f20c8c3a6
keelanfh <info@keelanfh.com>
2017-01-10 16:24:52 +0000

    twitter

--- debateScrape/debateScrape/twitter-analysis-sentiment-time-graph.py
+++ debateScrape/debateScrape/twitter-analysis-sentiment-time-graph.py
@@ -50,40 +50,40 @@ for candidate in candidates:
                                                                                    1:-1]
 
     plt.style.use('ggplot')
-    fig = plt.figure(len(candidate))
+    fig = plt.figure()
     ax = fig.gca()
-
-    labels = ['Negative', 'Positive']
-    for labelno, data in enumerate([negative_results, positive_results]):
-        data2 = zip(*data)
-        ax.plot(data2[0], data2[1], label=labels[labelno], lw=2.5)
-
-    ax.legend()
-    ax.set_xlabel('month')
-    ax.yaxis.set_label_position('left')
-    ax.set_ylabel('Proportion of words in  dictionaries')
-    ax.set_title(title)
     ax.grid(b=False)
     ax.set_axis_bgcolor('white')
-    data2 = zip(*tweet_volume)
 
     ax2 = ax.twinx()
     ax2.grid(b=False)
-    ax2.set_ylabel('Tweet Volume')
     ax2.set_axis_bgcolor('white')
+
+    labels = ['Negative', 'Positive']
+    colors = ['red', 'green']
+    for labelno, data in enumerate([negative_results, positive_results]):
+        data2 = zip(*data)
+        ax.plot(data2[0], data2[1], label=labels[labelno], color=colors[labelno], lw=2.5)
+
+    ax.set_xlabel('month')
+    ax.set_ylabel('Proportion of words in  dictionaries')
+    ax.set_title(title)
+    ax.legend(loc='center right')
+
+    ax2.set_ylabel('Tweet Volume')
+
+    data2 = zip(*tweet_volume)
     ax2.bar(data2[0], data2[1], color='black', alpha=0.1, align='center')
 
-    plt.savefig(export_filename, format='svg')
-
-    print [item.get_text() for item in ax2.get_xticklabels(which='both')]
-    labels = [int(item.get_text()) for item in ax2.get_xticklabels()]
-    labels2 = labels
-    for label in labels:
-        if label in months:
-            labels2[label] = months[label][0:3]
-        else:
-            labels2[label] = ""
-
-    ax2.set_xticklabels(labels2)
+    # print [item.get_text() for item in ax2.get_xticklabels(which='both')]
+    # labels = [int(item.get_text()) for item in ax2.get_xticklabels()]
+    # labels2 = labels
+    # for label in labels:
+    #     if label in months:
+    #         labels2[label] = months[label][0:3]
+    #     else:
+    #         labels2[label] = ""
+    #
+    # ax2.set_xticklabels(labels2)
 
     plt.savefig(export_filename, format='svg')
