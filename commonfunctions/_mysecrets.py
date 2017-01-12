082b54daf45d107956a7aa0512132cc5ac553ba0
keelanfh <info@keelanfh.com>
2017-01-12 00:41:14 +0000

    winner & loser graphs - first attempt

---
+++ commonfunctions/_mysecrets.py
@@ -0,0 +1,10 @@
+# Enter your Twitter API keys here and rename the file to mysecrets.py to use.
+
+
+class Secrets:
+
+    def __init__(self):
+        self.consumer_key = ''
+        self.consumer_secret = ''
+        self.access_token = ''
+        self.access_secret = ''
